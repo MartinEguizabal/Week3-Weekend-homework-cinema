0b9a9e188fa3970b67516cd36bc9859e231abce5
MartyCodeClan <zabbalab@hotmail.com>
2017-06-10 14:55:41 +0100

    1st commit

---
+++ models/customer.rb
@@ -0,0 +1,19 @@
+require_relative("../db/sql_runner")
+
+class Customer
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers (name, funds) VALUES ('#{@name}', '#{funds}') RETURNING id"
+    customer = SqlRunner.run(sql).first
+    @id = customer['id'].to_i
+  end
+
+  def 
+
+end
